75c2314174cdb56b6f4aa60edcd351f1cdc2fb04
mkulinski <me@michaelkulinski.com>
2016-06-08 13:01:48 -0700

    init commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    $(".shape1").click(function() {
+       $("#sound1").get(0).play();
+    });
+
+    $(".shape2").click(function() {
+        $("#sound2").get(0).play();
+    });
+
+    $(".shape3").click(function() {
+        $("#sound3").get(0).play();
+    });
+
+    $(".shape4").click(function() {
+        $("#sound4").get(0).play();
+    });
+
+});
