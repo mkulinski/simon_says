198a4412e819c8ff5f0bf1c191d3070c427a1c0a
Michael Kulinski <me@michaelkulinski.com>
2016-06-09 15:26:45 -0700

    added color flash when buttons are pressed

--- js/app.js
+++ js/app.js
@@ -2,18 +2,54 @@ $(document).ready(function() {
 
     $(".shape1").click(function() {
        $("#sound1").get(0).play();
+
+        var $el = $(".shape1"),
+            x = 100,
+            originalColor = $el.css("background");
+
+        $el.css("background", "#32CD32");
+        setTimeout(function(){
+            $el.css("background", originalColor);
+        }, x);
     });
 
     $(".shape2").click(function() {
         $("#sound2").get(0).play();
+
+        var $el = $(".shape2"),
+            x = 100,
+            originalColor = $el.css("background");
+
+        $el.css("background", "#ff0080");
+        setTimeout(function(){
+            $el.css("background", originalColor);
+        }, x);
     });
 
     $(".shape3").click(function() {
         $("#sound3").get(0).play();
+
+        var $el = $(".shape3"),
+            x = 100,
+            originalColor = $el.css("background");
+
+        $el.css("background", "#ccff00");
+        setTimeout(function(){
+            $el.css("background", originalColor);
+        }, x);
     });
 
     $(".shape4").click(function() {
         $("#sound4").get(0).play();
+
+        var $el = $(".shape4"),
+            x = 100,
+            originalColor = $el.css("background");
+
+        $el.css("background", "#00BFFF");
+        setTimeout(function(){
+            $el.css("background", originalColor);
+        }, x);
     });
 
 });
